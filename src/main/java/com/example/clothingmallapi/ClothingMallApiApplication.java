35257bb9428a0636d74e3cec22f1546cdea8eb3c
LSH <fhzktm@naver.com>
2024-03-16 14:35:16 +0900

    Initial commit

---
+++ src/main/java/com/example/clothingmallapi/ClothingMallApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothingmallapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingMallApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothingMallApiApplication.class, args);
+    }
+
+}
